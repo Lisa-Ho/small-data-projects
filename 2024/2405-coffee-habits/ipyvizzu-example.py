a45f0ae2cefb9355f616b50956ffaedcae6a8b69
Lisa-Ho <lisa_hornung@gmx.de>
2024-06-29 17:35:16 +0100

    explore ipyvizzu + streamlit

---
+++ 2024/2405-coffee-habits/ipyvizzu-example.py
@@ -0,0 +1,127 @@
+# import streamlit, pandas and ipyvizzu
+
+ 
+
+from streamlit.components.v1 import html
+
+import pandas as pd
+
+from ipyvizzu import Chart, Data, Config, Style, DisplayTarget
+
+ 
+
+ 
+
+def create_chart():
+
+    # initialize Chart
+
+ 
+
+    chart = Chart(
+
+        width="640px", height="360px", display=DisplayTarget.MANUAL
+
+    )
+
+ 
+
+    # create and add data to Chart
+
+ 
+
+    data = Data()
+
+    df = pd.read_csv(
+
+        "https://ipyvizzu.vizzuhq.com/0.17/showcases/titanic/titanic.csv"
+
+    )
+
+    data.add_df(df)
+
+ 
+
+    chart.animate(data)
+
+ 
+
+    # add config to Chart
+
+ 
+
+    chart.animate(
+
+        Config(
+
+            {
+
+                "x": "Count",
+
+                "y": "Sex",
+
+                "label": "Count",
+
+                "title": "Passengers of the Titanic",
+
+            }
+
+        )
+
+    )
+
+    chart.animate(
+
+        Config(
+
+            {
+
+                "x": ["Count", "Survived"],
+
+                "label": ["Count", "Survived"],
+
+                "color": "Survived",
+
+            }
+
+        )
+
+    )
+
+    chart.animate(Config({"x": "Count", "y": ["Sex", "Survived"]}))
+
+ 
+
+    # add style to Chart
+
+ 
+
+    chart.animate(Style({"title": {"fontSize": 35}}))
+
+ 
+
+    # return generated html code
+
+ 
+
+    return chart._repr_html_()
+
+ 
+
+ 
+
+# generate Chart's html code
+
+ 
+
+CHART = create_chart()
+
+ 
+
+ 
+
+# display Chart
+
+ 
+
+html(CHART, width=650, height=370)
---
+++ 2024/2405-coffee-habits/ipyvizzustory-example.py
@@ -0,0 +1,50 @@
+# import ipyvizzu and ipyvizzu-story
+from ipyvizzu import Data, Config
+from ipyvizzustory import Slide, Step
+from ipyvizzustory import Story  # or
+
+# from ipyvizzustory.env.st.story import Story
+
+# create data and initialize Story with the created data
+data = Data()
+data.add_series("Foo", ["Alice", "Bob", "Ted"])
+data.add_series("Bar", [15, 32, 12])
+data.add_series("Baz", [5, 3, 2])
+
+# you can also add data with pandas
+
+# import pandas as pd
+# data = Data()
+# df = pd.read_csv(
+#     "https://ipyvizzu-story.vizzuhq.com/0.10/assets/data/data.csv"
+# )
+# data.add_df(df)
+
+story = Story(data=data)
+
+# create Slides and Steps and add them to the Story
+slide1 = Slide(
+    Step(
+        Config({"x": "Foo", "y": "Bar"}),
+    )
+)
+story.add_slide(slide1)
+
+slide2 = Slide(
+    Step(Config({"color": "Foo", "x": "Baz", "geometry": "circle"}))
+)
+story.add_slide(slide2)
+
+# note: in Streamlit if you want to use the `play` method,
+# you need to set the width and height in pixels
+story.set_size(width="800px", height="480px")
+
+# you can export the Story into a html file
+#story.export_to_html(filename="mystory.html")
+
+# or you can get the html Story as a string
+html = story.to_html()
+print(html)
+
+# you can display the Story with the `play` method
+story.play()
