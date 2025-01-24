93e0a576e48fa1664967f4ee130c1fe9bea00aeb
Lisa-Ho <lisa_hornung@gmx.de>
2025-01-24 12:40:15 +0000

    started svelte app for publishing visuals

---
+++ 2025/2501-cheese-svelte/src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<main>
+
+<div id="title-container">
+<h1 class="title">Cheddar Shades and Creamy Hues</h1>
+<h2 class="subtitle">
+  A <span class="colorful">
+    <span class="c">c</span>
+    <span class="o">o</span>
+    <span class="l1">l</span>
+    <span class="o2">o</span>
+    <span class="r">r</span>
+    <span class="f">f</span>
+    <span class="u">u</span>
+    <span class="l2">l</span>
+  </span>
+  journey through cheese
+</h2>
+</div>
+
+<div id="intro-content">
+<p>When we think of cheese, we're most likely to picture something yellow. But is yellow the most common cheese color? Where does the most colorful cheese come from? And how does it differ by type of cheese?</p>
+<p>We will find out by analysing 1000+ cheese varieties worldwide.</p>
+
+</div>
+
+</main>
