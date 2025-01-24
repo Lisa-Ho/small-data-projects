f273335c1d12ad787fb8efc0bbc05918273ad509
Lisa-Ho <lisa_hornung@gmx.de>
2025-01-24 16:48:54 +0000

    setup hero component

---
+++ 2025/2501-cheese-svelte/src/components/Hero.svelte
@@ -0,0 +1,100 @@
+<script>
+</script>
+
+<div class="hero-background">
+  <div class="hero">
+    <h1 class="title">Cheddar Shades and Creamy Hues</h1>
+    <h2 class="subtitle">
+    A <span class="colorful">
+    <span class="c">c</span>
+    <span class="o">o</span>
+    <span class="l1">l</span>
+    <span class="o2">o</span>
+    <span class="r">r</span>
+    <span class="f">f</span>
+    <span class="u">u</span>
+    <span class="l2">l</span>
+    </span>
+    journey through cheese
+    </h2>
+    <p class="byline">
+      By <a href="https://inside-numbers.com/">Lisa Hornung</a>
+    </p>
+  </div>
+</div>
+
+<style>
+
+.hero {
+    max-width: 45rem;
+    margin: auto;
+    text-align: center;
+    padding: 80px 0 80px 0;
+}
+
+.title {
+    font-family: 'Mansalva', serif;
+    font-weight: 400; 
+    font-size: 3.6rem;
+    margin-bottom: 50px;  
+    color: #F7F7F7; 
+    text-transform: uppercase;
+} 
+
+.subtitle {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 300; 
+    font-size: 2.1rem;
+    margin-bottom: 25px; 
+    color: #F7F7F7; 
+} 
+
+
+/* Styling for the word 'colorful' */
+.colorful {
+    font-weight: 800;
+    display: inline-block;
+    letter-spacing: -1.5px; 
+    margin: 0 8px 0 8px;
+    /*background-color: #171717;*/
+    padding: 0 0 0 0;
+    border-radius: 4px;
+}
+  
+/*color pattern for each letter of 'colorful' */
+.colorful span {
+    display: inline-block;
+} 
+.colorful .c {
+    color: #E4CD6F;
+}
+  
+.colorful .o {
+    color: #FFB400;
+}
+  
+.colorful .l1 {
+    color: #E2B95A;
+}
+  
+.colorful .o2 {
+    color: #FFD000;
+}
+  
+.colorful .r {
+    color: #FFE714;
+}
+  
+.colorful .f {
+    color: #FFEE8F;
+}
+  
+.colorful .u {
+    color: #6EB3D5;
+}
+  
+.colorful .l2 {
+    color: #FFFDC7;
+}
+
+</style>
